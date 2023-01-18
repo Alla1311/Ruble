5b2d1cc8a26cec5d9f5c9dfb4249667b6ed0d7fc
Alla <lialia13111989@mail.ru>
2023-01-18 22:05:15 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int replenishment = 1000;
+
+        int bonus;
+        if (replenishment < 1000) {
+            bonus = 0;
+
+        } else {
+
+            bonus = replenishment / 100;
+        }
+        balance = balance + replenishment + bonus;
+
+                System.out.println(" Количество бонусов: " + bonus);
+        System.out.println(" Итоговый баланс: " + balance);
+
+    }
+}
